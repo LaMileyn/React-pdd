7907f78c815a548d13b62363be2b38a91c6b0591
Volg <lost-is-died@mail.ru>
2022-07-09 17:01:42 +0500

    (Result:page): - Завершена верстка страницы

--- src/views/Result/ResultsMistakes/ResultMistakes.tsx
+++ src/views/Result/ResultsMistakes/ResultMistakes.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import styles from './ResultsMistakes.module.scss';
 import cn from 'classnames';
+import TicketQuestionArea from "../../../components/TicketQuestionArea/TicketQuestionArea";
 
 const ResultMistakes: FC = (props) => {
     const hasMistakes = true
@@ -9,20 +10,25 @@ const ResultMistakes: FC = (props) => {
             {
                 !hasMistakes
                     ? (
-                        <div className={cn(styles.headline,styles.headlineSuccess)}>
+                        <div className={cn(styles.headline, styles.headlineSuccess)}>
                             <h2>Вы не допустили не одной ошибки</h2>
                         </div>
                     )
                     : (
                         <>
-                            <div className={cn(styles.headline,styles.headlineFail)}>
+                            <div className={cn(styles.headline, styles.headlineFail)}>
                                 <h2>Вы допустили 25 ошибок</h2>
                                 <span>Прорешать ошибки</span>
                             </div>
                             <div className={styles.mistake__items}>
-                                <div className={styles.mistake__item}>
-
-                                </div>
+                                {
+                                    Array(5).fill(null)
+                                        .map((el, index) => (
+                                            <div key={index} className={styles.mistake__item}>
+                                                <TicketQuestionArea/>
+                                            </div>
+                                        ))
+                                }
                             </div>
                         </>
                     )
